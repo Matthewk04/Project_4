bd231836300c8ce4877bb621bd5e82b9b76f0a5c
aheg7b <157038626+aheg7b@users.noreply.github.com>
2025-04-22 14:58:12 -0500

    worked on game.java

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,43 +1,86 @@
 package game;
 
 import maze.Grid;
-import player.Player;
+import maze.Row;
+import maze.Cell;
+import maze.CellComponents;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Game {
-	
-	private Grid grid;
+    
+    private Grid grid;
 
-	public Game(Grid grid) {
-		this.grid = grid;
-	}
+    // Constructor accepting a grid
+    public Game(Grid grid) {
+        this.grid = grid;
+    }
 
-	public Game(int i) {
-		if (size < 3 || size > 7) {
-			this.grid = null;
-		}else {
-			this.grid=createRandomGrid(size);
-		}
-	}
+    // Constructor that creates a random grid based on the provided size
+    public Game(int size) {
+        if (size < 3 || size > 7) {
+            this.grid = null; // Invalid size
+        } else {
+            this.grid = createRandomGrid(size); // Valid size, create random grid
+        }
+    }
 
-	public Grid createRandomGrid(int i) {
-		if (size<3||size>7) return null;
-		
-		Random rand = new Random
-	}
+    // Method to generate a random grid of the given size
+    public Grid createRandomGrid(int size) {
+        if (size < 3 || size > 7) return null;
 
-	public Object getGrid() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        Random rand = new Random();
+        List<Row> rows = new ArrayList<>();
 
-	public Object play(Object action, Player player) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        for (int i = 0; i < size; i++) {
+            List<Cell> cells = new ArrayList<>();
+            for (int j = 0; j < size; j++) {
+                // Randomly assign cell components (just an example, can modify this logic)
+                CellComponents left = rand.nextBoolean() ? CellComponents.WALL : CellComponents.APERTURE;
+                CellComponents right = rand.nextBoolean() ? CellComponents.WALL : CellComponents.APERTURE;
+                CellComponents up = rand.nextBoolean() ? CellComponents.WALL : CellComponents.APERTURE;
+                CellComponents down = rand.nextBoolean() ? CellComponents.WALL : CellComponents.APERTURE;
+                cells.add(new Cell(left, right, up, down));
+            }
+            rows.add(new Row(cells));
+        }
+        return new Grid(rows); // Return the newly created grid
+    }
 
-	public void setGrid(Object object) {
-		// TODO Auto-generated method stub
-		
-	}
+    // Getter for the grid
+    public Grid getGrid() {
+        return grid;
+    }
 
+    // Method to perform a movement action for the player
+    public boolean play(Movement action, Player player) {
+        if (action == null || player == null) return false; // Invalid input check
+
+        // Handle different movement actions
+        switch (action) {
+            case UP:
+                return player.moveUp();
+            case DOWN:
+                return player.moveDown();
+            case LEFT:
+                return player.moveLeft();
+            case RIGHT:
+                return player.moveRight();
+            default:
+                return false;
+        }
+    }
+
+    // Setter for the grid (useful for replacing the grid)
+    public void setGrid(Grid grid) {
+        this.grid = grid;
+    }
+
+    // ToString method to return a string representation of the game
+    @Override
+    public String toString() {
+        return "Game [grid=" + grid + "]";
+    }
 }

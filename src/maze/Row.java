abb0f29fca24902a34928fefee6d43485cb71cf4
aheg7b <157038626+aheg7b@users.noreply.github.com>
2025-04-22 14:49:19 -0500

    worked on row.java

--- src/maze/Row.java
+++ src/maze/Row.java
@@ -1,16 +1,39 @@
 package maze;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Row {
 
-	public Row(ArrayList<Cell> cells) {
-		// TODO Auto-generated constructor stub
-	}
+    private ArrayList<Cell> cells;
 
-	public ArrayList<Cell> getCells() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Row(ArrayList<Cell> cells) {
+        this.cells = cells;
+    }
 
+    public ArrayList<Cell> getCells() {
+        return cells;
+    }
+
+    public void setCells(ArrayList<Cell> cells) {
+        this.cells = cells;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Row)) return false;
+        Row other = (Row) obj;
+        return Objects.equals(cells, other.cells);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cells);
+    }
+
+    @Override
+    public String toString() {
+        return "Row [cells=" + cells + "]";
+    }
 }

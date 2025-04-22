2f4875b4276b2d182af395d9151bb17896865055
aheg7b <157038626+aheg7b@users.noreply.github.com>
2025-04-22 14:51:26 -0500

    worked on cell.java

--- src/maze/Cell.java
+++ src/maze/Cell.java
@@ -1,63 +1,79 @@
 package maze;
 
-import java.util.ArrayList;
+import java.util.Objects;
 
 public class Cell {
-	
-	private CellComponents up;
-	private CellComponents down;
-	private CellComponents left;
-	private CellComponents right;
-	
 
-	public Cell(CellComponents up, CellComponents down, CellComponents left, CellComponents right) {
-		this.up = up;
-		this.down = down;
-		this.left = left;
-		this.right = right;
-	}
+    private CellComponents left;
+    private CellComponents right;
+    private CellComponents up;
+    private CellComponents down;
 
-	public ArrayList<Cell> getCells() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Cell(CellComponents left, CellComponents right, CellComponents up, CellComponents down) {
+        this.left = left;
+        this.right = right;
+        this.up = up;
+        this.down = down;
+    }
 
-	public CellComponents getUp() {
-		return up;
-	}
+    public CellComponents getLeft() {
+        return left;
+    }
 
-	public void setUp(CellComponents up) {
-		this.up = up;
-	}
+    public void setLeft(CellComponents left) {
+        if (left != null) {
+            this.left = left;
+        }
+    }
 
-	public CellComponents getDown() {
-		return down;
-	}
+    public CellComponents getRight() {
+        return right;
+    }
 
-	public void setDown(CellComponents down) {
-		this.down = down;
-	}
+    public void setRight(CellComponents right) {
+        if (right != null) {
+            this.right = right;
+        }
+    }
 
-	public CellComponents getLeft() {
-		return left;
-	}
+    public CellComponents getUp() {
+        return up;
+    }
 
-	public void setLeft(CellComponents left) {
-		this.left = left;
-	}
+    public void setUp(CellComponents up) {
+        if (up != null) {
+            this.up = up;
+        }
+    }
 
-	public CellComponents getRight() {
-		return right;
-	}
+    public CellComponents getDown() {
+        return down;
+    }
 
-	public void setRight(CellComponents right) {
-		this.right = right;
-	}
+    public void setDown(CellComponents down) {
+        if (down != null) {
+            this.down = down;
+        }
+    }
 
-	@Override
-	public String toString() {
-		return "Cell [up=" + up + ", down=" + down + ", left=" + left + ", right=" + right + "]";
-	}
+    @Override
+    public String toString() {
+        return "Cell [left=" + left + ", right=" + right + ", up=" + up + ", down=" + down + "]";
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Cell)) return false;
+        Cell other = (Cell) obj;
+        return left == other.left &&
+               right == other.right &&
+               up == other.up &&
+               down == other.down;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right, up, down);
+    }
 }
